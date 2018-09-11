def2aef2070a103cc90bea4f8ba6a5ceafdb3835
Pavol Hejny <me@pavolhejny.com>
2018-09-11 14:25:11 +0200

    svgTransform

--- src/tools/svgTools.ts
+++ src/tools/svgTools.ts
@@ -9,3 +9,46 @@ export function translateToVector(translate = 'translate(0,0)') {
     const [x, y] = inner.split(/,|\s+/);
     return new Vector2(parseFloat(x), parseFloat(y));
 }
+
+type TTranslate = [number,number]|null;
+type TRotate = [number,number,number]|null;
+
+export class svgTransform{
+
+    static TRANSLATE = /^translate\(\s*(\-?\d+)\s(\-?\d+)\s(\-?\d+)\s*\)$/g; 
+
+    static fromString(transform:string){
+
+        let translate:TTranslate = null;
+        let rotate:TRotate = null; 
+
+        transform.split(' ').map((part)=>{
+
+            if(this.TRANSLATE.test(part)){
+
+                translate = [1,1,1];
+
+            }else{
+                throw new Error(`Unknown part of svg transform "part".`);
+            }
+
+
+        })
+    }
+
+    constructor(private translate:TTranslate,private rotate:TRotate){
+    }
+
+    toString{
+
+    }
+
+}
+
+
+export function modifySvgTransform(type:'translate|rotate',addTo:number[]){
+
+
+
+
+}

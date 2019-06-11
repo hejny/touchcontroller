4fdbfe5c7c6284b7b670b2c2ce2bb7724f3eddf5
Pavol Hejný <me@pavolhejny.com>
2019-06-11 18:13:24 +0200

    expose server

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')  
 const serveStatic = require('serve-static')
+const serveIndex = require('serve-index')
 
 const staticBasePath = './';
 
-const app = express()
-const port = 3000;
+const app = express();
 
 app.use(serveStatic(staticBasePath, {'index': false}))  
-app.listen(port);
+app.use(serveIndex(staticBasePath, {'icons': true}))  
+app.listen(3000);
 
-console.log(`Static server listening on port ${port}.`);
+console.log(`Static server listening on port 3000.`);

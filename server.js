7e199b9df628cc73dca72d50634e17170dbc71c8
Pavol Hejny <me@pavolhejny.com>
2019-07-12 11:54:55 +0200

    Server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')  
 const serveStatic = require('serve-static')
 const serveIndex = require('serve-index')
+const opn = require('opn')
+
+// TODO: as indipendent library
+
+const PORT = 6012;
 
 const staticBasePath = './';
 
@@ -8,6 +13,7 @@ const app = express();
 
 app.use(serveStatic(staticBasePath, {'index': false}))  
 app.use(serveIndex(staticBasePath, {'icons': true}))  
-app.listen(3000);
+app.listen(PORT);
 
-console.log(`Static server listening on port 3000.`);
+console.log(`Static server listening on port ${PORT}.`);
+opn(`http://localhost:${PORT}/samples/`);

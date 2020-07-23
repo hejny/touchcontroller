5887ec70251a27f6c83ef459d1261dc459c11f89
Pavol Hejný <me@pavolhejny.com>
2020-07-23 19:34:27 +0200

    Test

---
+++ test/TouchController.test.js
@@ -0,0 +1,8 @@
+//import { TouchController } from '../src/TouchController';
+
+describe('TouchController', () => {
+    it('TouchController is working.', () => {
+        expect(true).toEqual(true);
+        // TODO: Implement
+    });
+});

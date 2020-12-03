24634a3af4e928be395c28e0ce8c9582a6ad94ec
Pavol Hejny <me@pavolhejny.com>
2020-12-03 01:06:39 +0100

    padArray

---
+++ src/utils/padArray.ts
@@ -0,0 +1,12 @@
+
+interface IPadArrayOptions<T>{
+    padWith: T|T[];
+    length: number;
+    crop?: boolean;
+}
+
+export function padArray<T>(array: T[],{padWith,length,crop}: IPadArrayOptions<T>): T[]{
+    return Array(crop===false?Math.max(length,array.length):Math.max(length,0)).fill(null).map((_,i)=>(
+        array[i]||(Array.isArray(padWith)?(padWith[(i-array.length)%padWith.length]):padWith)
+    ));
+}

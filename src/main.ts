329a0e39fbda344847f8f3c2f83059fa2e9c30a5
Pavol Hejny <me@pavolhejny.com>
2020-12-03 23:13:29 +0100

    Grid

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { emulateTouch } from './debug/emulateTouch';
 import { MultiTouchControllerDebugLayer } from './debug/MultiTouchControllerDebugLayer';
 import { TouchControllerDebugLayer } from './debug/TouchControllerDebugLayer';
 import { DrawController } from './drawController/CanvasParticlesRenderer';
-import { IParticleOptions, IParticleOptionsExternals, Particle } from './drawController/Particle';
+import { IParticleOptions } from './drawController/Particle';
+import { IParticleOptionsExternals } from './drawController/Particle';
+import { Particle } from './drawController/Particle';
 import { Scene } from './drawController/Scene';
 import { Awaitable } from './interfaces/Awaitable';
 import { IElement } from './interfaces/IElement';
@@ -17,55 +19,56 @@ import { TouchListener } from './listeners/TouchListener';
 import { MultiTouch } from './multitouch/MultiTouch';
 import { MultiTouchController } from './multitouch/MultiTouchController';
 import { multiTouchTransforms } from './multitouch/multiTouchTransforms';
-import { Grid } from './touch/Grid';
-import { GridTouchController } from './touch/GridTouchController';
 import { Touch } from './touch/Touch';
 import { TouchController } from './touch/TouchController';
 import { TouchFrame } from './touch/TouchFrame';
-import { average, particleOptionsAverage, TAverageItems, VectorAverage } from './utils/average';
+import { TAverageItems } from './utils/average';
+import { average } from './utils/average';
+import { VectorAverage } from './utils/average';
+import { particleOptionsAverage } from './utils/average';
 import { SourceCache } from './utils/Cache';
 import { CanvasRectangle } from './utils/CanvasRectangle';
 import { EventManager } from './utils/EventManager';
 import { getBoundingClientRectEnhanced } from './utils/getBoundingClientRectEnhanced';
-import { createCanvasFromSrc, createColoredCanvasFromSrc, createImageFromSrc } from './utils/imageTools';
+import { createImageFromSrc } from './utils/imageTools';
+import { createCanvasFromSrc } from './utils/imageTools';
+import { createColoredCanvasFromSrc } from './utils/imageTools';
 import { sign } from './utils/mathTools';
 import { padArray } from './utils/padArray';
 
 export {
-    sign,
-    Grid,
-    Touch,
-    Scene,
-    IEvent,
-    average,
-    padArray,
-    IElement,
-    Particle,
-    IListener,
-    Awaitable,
-    TouchFrame,
-    MultiTouch,
-    SourceCache,
-    EventManager,
-    emulateTouch,
-    VectorAverage,
-    TAverageItems,
-    TouchListener,
-    MouseListener,
-    DrawController,
-    CanvasRectangle,
-    TouchController,
-    ITouchController,
-    IParticleOptions,
-    createImageFromSrc,
-    createCanvasFromSrc,
-    GridTouchController,
-    multiTouchTransforms,
-    MultiTouchController,
-    particleOptionsAverage,
-    IParticleOptionsExternals,
-    TouchControllerDebugLayer,
-    createColoredCanvasFromSrc,
-    getBoundingClientRectEnhanced,
-    MultiTouchControllerDebugLayer
+sign,
+Touch,
+Scene,
+IEvent,
+average,
+padArray,
+IElement,
+Particle,
+IListener,
+Awaitable,
+TouchFrame,
+MultiTouch,
+SourceCache,
+EventManager,
+emulateTouch,
+VectorAverage,
+TAverageItems,
+TouchListener,
+MouseListener,
+DrawController,
+CanvasRectangle,
+TouchController,
+ITouchController,
+IParticleOptions,
+createImageFromSrc,
+createCanvasFromSrc,
+multiTouchTransforms,
+MultiTouchController,
+particleOptionsAverage,
+IParticleOptionsExternals,
+TouchControllerDebugLayer,
+createColoredCanvasFromSrc,
+getBoundingClientRectEnhanced,
+MultiTouchControllerDebugLayer
 };

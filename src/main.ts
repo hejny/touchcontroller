ddba4c6ce2916ce5978a2afa2164877e59b2459a
Pavol Hejny <me@pavolhejny.com>
2021-03-26 16:40:30 +0100

    Autofixing

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,13 @@
 import { MultitouchControllerDebugLayer } from './debug/MultiTouchControllerDebugLayer';
 import { TouchControllerDebugLayer } from './debug/TouchControllerDebugLayer';
 import { DrawController } from './drawController/CanvasParticlesRenderer';
-import { IParticleOptions } from './drawController/Particle';
-import { IParticleOptionsExternals } from './drawController/Particle';
-import { Particle } from './drawController/Particle';
+import { IParticleOptions, IParticleOptionsExternals, Particle } from './drawController/Particle';
 import { Scene } from './drawController/Scene';
 import { emulateTouch } from './emulate/emulateTouch';
-import { IEmulateTouchOptions } from './emulate/IEmulateTouchOptions';
-import { IEmulateTouchOptionsAdvanced } from './emulate/IEmulateTouchOptions';
+import { IEmulateTouchOptions, IEmulateTouchOptionsAdvanced } from './emulate/IEmulateTouchOptions';
 import { IAwaitable } from './interfaces/IAwaitable';
 import { IDestroyable } from './interfaces/IDestroyable';
-import { IElement } from './interfaces/IElement';
-import { IElementListeners } from './interfaces/IElement';
+import { IElement, IElementListeners } from './interfaces/IElement';
 import { IEvent } from './interfaces/IEvent';
 import { IListener } from './interfaces/IListener';
 import { ITouchController } from './interfaces/ITouchController';
@@ -22,69 +18,63 @@ import { MouseListener } from './listeners/MouseListener';
 import { TouchListener } from './listeners/TouchListener';
 import { Multitouch } from './multitouch/MultiTouch';
 import { MultitouchController } from './multitouch/MultiTouchController';
-import { IMultitouchTransformsOptions } from './multitouch/multiTouchTransforms/multiTouchTransforms';
-import { multitouchTransforms } from './multitouch/multiTouchTransforms/multiTouchTransforms';
+import { IMultitouchTransformsOptions, multitouchTransforms } from './multitouch/multiTouchTransforms/multiTouchTransforms';
 import { multitouchTransformsOnElement } from './multitouch/multiTouchTransforms/multitouchTransformsOnElement';
 import { Touch } from './touch/Touch';
 import { TouchController } from './touch/TouchController';
 import { TouchFrame } from './touch/TouchFrame';
-import { TAverageItems } from './utils/average';
-import { average } from './utils/average';
-import { VectorAverage } from './utils/average';
-import { particleOptionsAverage } from './utils/average';
+import { average, particleOptionsAverage, TAverageItems, VectorAverage } from './utils/average';
 import { SourceCache } from './utils/Cache';
 import { CanvasRectangle } from './utils/CanvasRectangle';
 import { EventManager } from './utils/EventManager';
 import { getBoundingClientRectEnhanced } from './utils/getBoundingClientRectEnhanced';
-import { createImageFromSrc } from './utils/imageTools';
-import { createCanvasFromSrc } from './utils/imageTools';
-import { createColoredCanvasFromSrc } from './utils/imageTools';
+import { createCanvasFromSrc, createColoredCanvasFromSrc, createImageFromSrc } from './utils/imageTools';
 import { sign } from './utils/mathTools';
 import { Omit } from './utils/Omit';
 import { padArray } from './utils/padArray';
 import { WithOptional } from './utils/WithOptional';
 
 export {
-Omit,
-sign,
-Touch,
-Scene,
-IEvent,
-average,
-padArray,
-IElement,
-Particle,
-IListener,
-TouchFrame,
-Multitouch,
-IAwaitable,
-SourceCache,
-WithOptional,
-EventManager,
-IDestroyable,
-emulateTouch,
-VectorAverage,
-TAverageItems,
-TouchListener,
-MouseListener,
-DrawController,
-CanvasRectangle,
-TouchController,
-ITouchController,
-IParticleOptions,
-IElementListeners,
-createImageFromSrc,
-createCanvasFromSrc,
-multitouchTransforms,
-MultitouchController,
-IEmulateTouchOptions,
-particleOptionsAverage,
-IParticleOptionsExternals,
-TouchControllerDebugLayer,
-createColoredCanvasFromSrc,
-IMultitouchTransformsOptions,
-IEmulateTouchOptionsAdvanced,
-getBoundingClientRectEnhanced,
-multitouchTransformsOnElement,
-MultitouchControllerDebugLayer
+    Omit,
+    sign,
+    Touch,
+    Scene,
+    IEvent,
+    average,
+    padArray,
+    IElement,
+    Particle,
+    IListener,
+    TouchFrame,
+    Multitouch,
+    IAwaitable,
+    SourceCache,
+    WithOptional,
+    EventManager,
+    IDestroyable,
+    emulateTouch,
+    VectorAverage,
+    TAverageItems,
+    TouchListener,
+    MouseListener,
+    DrawController,
+    CanvasRectangle,
+    TouchController,
+    ITouchController,
+    IParticleOptions,
+    IElementListeners,
+    createImageFromSrc,
+    createCanvasFromSrc,
+    multitouchTransforms,
+    MultitouchController,
+    IEmulateTouchOptions,
+    particleOptionsAverage,
+    IParticleOptionsExternals,
+    TouchControllerDebugLayer,
+    createColoredCanvasFromSrc,
+    IMultitouchTransformsOptions,
+    IEmulateTouchOptionsAdvanced,
+    getBoundingClientRectEnhanced,
+    multitouchTransformsOnElement,
+    MultitouchControllerDebugLayer
 };

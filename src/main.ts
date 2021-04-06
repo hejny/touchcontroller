5dce133f3ba56f74cacd73be8a156bbfe2708224
Pavol Hejny <me@pavolhejny.com>
2021-04-06 13:07:26 +0200

    Updating exports

--- src/main.ts
+++ src/main.ts
@@ -74,5 +74,5 @@ export {
     IEmulateTouchOptionsAdvanced,
     getBoundingClientRectEnhanced,
     multitouchTransformsOnElement,
-    MultitouchControllerDebugLayer,
+    MultitouchControllerDebugLayer
 };
